52765a6a544a948339234afd97497c99ccc30d18
xieqihui41 <2309215972@qq.com>
2019-08-12 13:46:55 +0800

    新增控制器.com/example/demo/controller/IndexController.java 页面.index.html;404.html;hello.html

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,27 @@
+/**
+ *  Copyright(C) 2015 Suntec Software(Shanghai) Co., Ltd.
+ *  All Right Reserved.
+ */
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Descriptions
+ *
+ * @version 2019年8月12日
+ * @author SUNTEC
+ * @since JDK1.8
+ *
+ */
+//@RestController 返回数值
+@Controller
+public class IndexController {
+	
+	@GetMapping("index")
+	public String index() {
+		return "hello";
+	}
+}
